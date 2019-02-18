3750562d3854b6f2afc0dd2076b9c345840daf4b
Gregoire Courtois <courtoisgregoire@gmail.com>
2019-02-18 17:11:17 +0100

    user model done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
+  validates :email, presence: true, uniqueness: true, allow_blank: false
+  validates :rating, numericality: true
 end
