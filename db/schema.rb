a0fff74920168df558d0758d19ee058ccd33469e
Pierre Lestang <plestang@gmail.com>
2019-02-18 17:57:09 +0100

    ras

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_18_154037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "boats", force: :cascade do |t|
+    t.integer "price_per_day"
+    t.string "category"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,14 +37,6 @@ ActiveRecord::Schema.define(version: 2019_02_18_154037) do
     t.float "rating"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end 
-  
-  create_table "boats", force: :cascade do |t|
-    t.integer "price_per_day"
-    t.string "category"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
