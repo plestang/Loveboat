ac5f71e5215563c441d342dd303793956177fe6c
Pierre Lestang <plestang@gmail.com>
2019-02-19 11:20:30 +0100

    pubndit

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,18 +1,25 @@
 class BoatsController < ApplicationController
+  before_action :set_boat, only: [:show, :edit, :update, :destroy]
+
   def index
     @boats = Boat.all
   end
 
   def show
-    @boat = Boat.find(params[:id])
+    authorize @boat
   end
 
   def new
     @boat = Boat.new
+    authorize @boat
+  end
+
+  def edit
   end
 
   def create
-     @boat = Boat.new(boat_params)
+    @boat = Boat.new(boat_params)
+    authorize @boat
     if @boat.save
       redirect_to boat_path(@boat)
     else
@@ -20,9 +27,25 @@ class BoatsController < ApplicationController
     end
   end
 
+  def update
+    authorize @boat
+    @boat.update(boat_params)
+  end
+
+  def destroy
+    authorize @boat
+    @boat.destroy
+    redirect_to boats_path
+  end
+
   private
 
   def boat_params
     params.require(:boat).permit(:price_per_day, :category, :description)
   end
+
+  def set_boat
+    @boat = Boat.find(params[:id])
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+  skip_before_action :authenticate_user!
+
   def home
   end
 end
