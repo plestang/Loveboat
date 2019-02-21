325247665b3d048fcaa5ca96fb60fd9b85c13b4e
Pierre Lestang <plestang@gmail.com>
2019-02-21 12:11:24 +0100

    db updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_142244) do
+ActiveRecord::Schema.define(version: 2019_02_21_101631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2019_02_19_142244) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.bigint "user_id"
+    t.string "name"
+    t.integer "capacity"
+    t.integer "cabin"
+    t.integer "length"
+    t.string "address"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
